a8a09c845088a7eb30b797c3729feac96563c872
hayssakroin <161839031+hayssakroin@users.noreply.github.com>
2025-02-24 11:56:22 -0300

    Add files via upload

---
+++ hayssa/aula1.js
@@ -0,0 +1,13 @@
+//calculadora em javaScript
+const valor1= 1;
+const valor2= 2;
+let soma = valor1 + valor2;
+let sub = valor1 - valor2;
+let mult = valor * valor2;
+let div = valor1 / valor2;
+
+console.log("A soma é: " + soma);
+console.log("A subtração é: " + sub);
+console.log("A multiplicação é: " + mult);
+console.log("A divisão é: " + div);
+//node nome-arquivo.js
---
+++ hayssa/condicao.js
@@ -0,0 +1,7 @@
+let idade = 18
+let possuiCNH = true
+if(idade >= 18 && possuiCNH == true){
+    console.log("você poe dirigir!")
+} else {
+    console.log("ande de onibus ou uber!")
+}
