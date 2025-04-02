5ee543fea05e87a84114dddc6d7ece8de474e738
hayssakroin <161839031+hayssakroin@users.noreply.github.com>
2025-04-02 11:42:57 -0300

    Add files via upload

---
+++ aula.js/exercicio.js
@@ -0,0 +1,56 @@
+let livros = [
+    { 
+        titulo: "A Biblioteca da Meia-Noite",
+        autor: "Matt Haig",
+        editora: "Matt Haig",
+        ano: 2020 , 
+        sinopse: "conta a história de Nora Seed, uma mulher que se arrepende das escolhas que fez na vida. ",
+        paginas: 37 ,
+        genero:["romance"],
+        preco: 47,00
+    },
+
+    { 
+        titulo: "Café com deus pai 2025",
+        autor: "Junior Rostirola",
+        editora: "Velos ",
+        ano: 2025, 
+        sinopse: " Porções Diárias de Transformação é um livro que propõe uma jornada de devocionais diários ao longo do ano. ",
+        paginas: 70 ,
+        genero:[""],
+        preco: 78,00
+    },
+
+    { 
+        titulo: "Ainda estou aqui",
+        autor: "Walter Salles",
+        editora: "Marcelo Rubens Paiva",
+        ano: 2024 , 
+        sinopse: "No Rio de Janeiro, a família Paiva - Rubens, Eunice e seus cinco filhos - vive à beira da praia em uma casa de portas abertas para os amigos.",
+        paginas: ,
+        genero:[""],
+        preco:
+    },
+
+    { 
+        titulo: "",
+        autor: "",
+        editora: "",
+        ano: , 
+        sinopse: "",
+        paginas: ,
+        genero:[""],
+        preco:
+    },
+
+    { 
+        titulo: "",
+        autor: "",
+        editora: "",
+        ano: , 
+        sinopse: "",
+        paginas: ,
+        genero:[""],
+        preco:
+    },
+]
---
+++ aula.js/objetos.js
@@ -0,0 +1,22 @@
+//objetos
+let lanches = [
+    {
+        nome: "x-burguer",
+        preco: 50,
+        ingredientes: ["pão", "queijo", "hambúrguer"]
+    },
+   
+    {
+        nome: "x-salada",
+        preco: 30,
+        ingredientes: ["pão", "queijo", "hambúrguer", "salada"]
+    },
+
+    {
+        nome: "x-tudo",
+        preco: 300,
+        ingredientes: ["tudo"]
+    },
+]
+console.log("nome: " + lanche.nome + "- preço: " + lanche.preco);
+console.log("ingredientes: " + lanche.ingredientes);
