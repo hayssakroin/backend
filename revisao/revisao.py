0b8993021e5e2398fe1230d53c016db265feaac3
hayssakroin <161839031+hayssakroin@users.noreply.github.com>
2025-05-12 12:18:35 -0300

    Add files via upload

---
+++ revisao/revisao.py
@@ -0,0 +1,20 @@
+nome = "Hayssa"
+altura = 1.60
+musica_favorita = "the box"
+serie_favorita = "game of thrones"
+
+
+
+print ("## BEM VINDO AO SISTEMA! ##")
+print ("Sistema criando por Aluna. Hayssa , n° chamada: 12")
+
+opcao = int(input("qual sua altura?"))
+if(altura >= 1.60):
+    print("você é maior que eu!")
+
+if( altura <= 1.60):
+    print("você é menor que eu!")
+
+
+
+
